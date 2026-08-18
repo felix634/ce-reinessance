0a529b755ab820afbb347a19e136ad2774082ac3
Felix Bary <felixbary@gmail.com>
2026-08-18 11:19:29 +0200

    Redesign CE Reinessance website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // <-- Correctly import the Astro integration
 
-// https://astro.build/config
-export default defineConfig({
-  // Use the standard 'integrations' array for Astro plugins
-  integrations: [tailwind()], 
-});
+export default defineConfig({});
